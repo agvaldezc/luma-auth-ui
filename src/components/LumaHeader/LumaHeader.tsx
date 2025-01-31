667f0acb185f66fb07a2bd970b52d95e0bd91ed1
Alan Valdez <agvaldezc@gmail.com>
2025-01-31 16:14:21 -0600

    Removed margin classes

--- src/components/LumaHeader/LumaHeader.tsx
+++ src/components/LumaHeader/LumaHeader.tsx
@@ -5,8 +5,8 @@ export const LumaHeader = () => {
   return (
     <header className="flex h-16 px-4 py-4 items-center">
       <div id="heading-group" className="flex gap-2 items-center h-full">
-        <SidebarTrigger className='-ml-1' />
-        <Separator orientation="vertical" className='-ml-1' />
+        <SidebarTrigger />
+        <Separator orientation="vertical" />
         <h1 className="font-bold text-2xl">Header</h1>
       </div>
     </header>
